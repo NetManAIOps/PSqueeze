e9471f1aeb6b55733203a4fb09774b9f4f17fcd9
chenyihao <chenyiha17@mails.tsinghua.edu.cn>
2020-02-07 11:48:21 +0800

    update with external root cause dataset

--- data/data_gen.py
+++ data/data_gen.py
@@ -18,22 +18,28 @@ def make_dir(path, remove_flag=0):
     if not isExists:
         os.makedirs(path)
 
-def parse_groundtruth(gt_str):
-    def parse_one(s):
-        return dict([i.split("=") for i in s])
-    gt_list = [i.split('&') for i in gt_str.split(";")]
-    gt_list = map(parse_one, gt_list)
-    return list(gt_list)
-
 def group_for(timestamp, ex_rc, input_path, output_dir):
-    df = pd.read_csv(input_path / f"{timestamp}.csv", engine='python', dtype='str', delimiter=r"\s*,\s*")
-    ex_rc = parse_groundtruth(ex_rc)
-    ex_keys = reduce(lambda x,y: x.extend(y), map(lambda t: t.keys(), ex_rc))
-    keys = sorted(list(set(df.columns.values) - set(ex_keys)))
-    
-    df.to_csv(output_dir / f"{timestamp}.csv", index=False, columns=keys)
+    source_file = input_path / f"{timestamp}.csv"
+    target_file = output_dir / f"{timestamp}.csv"
+    if type(ex_rc) == type(None):
+        shutil.copyfile(source_file, target_file)
+        return
 
-def gen_exrc_data_for(dataset, n_elements, cuboid_layer, n_ex_rc=1, n_workers=20):
+    df = pd.read_csv(source_file, engine='python', dtype='str', delimiter=r"\s*,\s*")
+    keys = sorted(list(set(df.columns.values) - set(ex_rc)))
+    
+    df.to_csv(target_file, index=False, columns=keys)
+
+def get_rc_dim(rc_str, n_ex_dim):
+    rc_dim = list(map(lambda x: x.split("&"), rc_str.split(";")))
+    rc_dim = [item for sublist in rc_dim for item in sublist]
+    ret = list(set(map(lambda x: x.split("=")[0], rc_dim)))
+    return ret[:min(n_ex_dim, len(ret))]
+
+def get_rc_dim_wrapper(n_ex_dim):
+    return lambda x: get_rc_dim(x, n_ex_dim)
+
+def gen_exrc_data_for(dataset, n_elements, cuboid_layer, n_ex_dim=1, n_workers=20, ex_rc_ratio=0.5):
     if dataset.find("A_week") != -1:
         setting = f"new_dataset_{dataset}_n_elements_{n_elements}_layers_{cuboid_layer}"
         input_path = SCRIPT_DIR / "A" / setting
@@ -43,14 +49,22 @@ def gen_exrc_data_for(dataset, n_elements, cuboid_layer, n_ex_rc=1, n_workers=20
         setting = f"B_cuboid_layer_{cuboid_layer}_n_ele_{n_elements}"
         input_path = SCRIPT_DIR / dataset / setting
         output_dir = SCRIPT_DIR / "E" / dataset / setting
+
     injection_info = pd.read_csv(input_path / 'injection_info.csv', engine='c')
     rc = injection_info["set"].values
     timestamps = injection_info['timestamp'].values
-    ex_rc = list(map(
-        lambda x: ";".join(x.split(";")[:min(len(x.split(";")), int(n_ex_rc))]),
-        rc
-    ))
-    injection_info["ex_rc"] = ex_rc
+
+    ex_rc = np.array(list(map(get_rc_dim_wrapper(int(n_ex_dim)), rc)), dtype=object)
+
+    mask = np.arange(rc.size)
+    np.random.shuffle(mask)
+    mask = mask[:int(mask.size*ex_rc_ratio)]
+    ex_rc[mask] = None
+    def ex_rc_to_str(s):
+        if s[0]: return ";".join(sorted(s))
+        else: return None
+    injection_info["ex_rc_dim"] = [ex_rc_to_str(i) for i in ex_rc.tolist()]
+
     make_dir(output_dir.resolve(), remove_flag=1)
     injection_info.to_csv(output_dir / "injection_info.csv", index=False)
 
